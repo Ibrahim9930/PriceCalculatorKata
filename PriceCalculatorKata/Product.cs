4c73e3cbd11e29487a799f36f08bd01cb04d6523
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-06-22 15:32:55 +0300

    TAX : Add and implement the Product class

---
+++ PriceCalculatorKata/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PriceCalculatorKata
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int UPC { get; }
+
+        private float _basePrice;
+        public float BasePrice
+        {
+            get
+            {
+                return _basePrice;
+            } set {
+                if(value > 0)
+                {
+                    _basePrice = (float)Math.Round(value, 2);
+                }   
+            }
+        }
+
+        public float PriceAfterTax
+        {
+            get
+            {
+                return BasePrice + (TaxPercentage / 100.0f) * BasePrice;
+            }
+        }
+        public int TaxPercentage { get; set; }
+
+        public Product(int UPC)
+        {
+            this.UPC = UPC;
+            this.TaxPercentage = 20;
+        }
+
+        public void Display(Action<string> displayMethod)
+        {
+            displayMethod($"{Name}'s price before tax : {BasePrice:0.00}$ and after a {TaxPercentage}% tax : {PriceAfterTax:0.00}$");
+        }
+
+
+    }
+}
