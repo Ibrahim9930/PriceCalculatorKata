6f31c6bb8a8fd5cfe37e8915012b05ba08d5b8a9
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-07-01 14:43:45 +0300

    SELECTIVE : Add tax interface and two implementations classes

---
+++ PriceCalculatorKata/Tax.cs
@@ -0,0 +1,39 @@
+﻿namespace PriceCalculatorKata.Tax
+{
+    public interface ITax
+    {
+        float getTax();
+    }
+
+    public class UniversalTax : ITax
+    {
+        private readonly float _tax;
+
+        public float getTax()
+        {
+            return _tax;
+        }
+    }
+
+    public class AllTaxes : ITax
+    {
+        private ITax[] _taxes;
+        private float _taxSummation;
+
+        public AllTaxes(ITax[] taxes)
+        {
+            _taxes = taxes;
+            _taxSummation = 0;
+        }
+        public float getTax()
+        {
+            foreach (var tax in _taxes)
+            {
+                _taxSummation += tax.getTax();
+            }
+
+            return _taxSummation;
+        }
+    }
+    
+}
