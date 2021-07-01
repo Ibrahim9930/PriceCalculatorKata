817d2b7a4387b5c06da80daa253cf53545b06d93
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-07-01 14:38:11 +0300

    SELECTIVE : Add discount interface and three implementation classes

---
+++ PriceCalculatorKata/Discount.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace PriceCalculatorKata.Dicsount
+{
+    public interface IDiscount
+    {
+        float getDiscount();
+    }
+    public class UniversalDiscount : IDiscount
+    {
+        private readonly float _discount;
+
+        public UniversalDiscount(float discount)
+        {
+            _discount = discount;
+        }
+        public float getDiscount()
+        {
+            return _discount;
+        }
+    }
+
+    public class UPCBasedDiscount : IDiscount
+    {
+        public static Dictionary<int, float> UPCDiscounts { get; private set; }
+        
+        private int _productUPC;
+
+        public UPCBasedDiscount(int productUPC)
+        {
+            _productUPC = productUPC;
+            UPCDiscounts = new Dictionary<int, float>();
+        }
+        public float getDiscount()
+        {
+            return UPCDiscounts.GetValueOrDefault(_productUPC);
+        }
+    }
+
+    public class AllDiscounts : IDiscount
+    {
+        private IDiscount[] _discounts;
+        private float _discountSummation;
+        
+        public float getDiscount()
+        {
+            foreach (var discount in _discounts)
+            {
+                _discountSummation += discount.getDiscount();
+            }
+
+            return _discountSummation;
+        }
+    }
+}
