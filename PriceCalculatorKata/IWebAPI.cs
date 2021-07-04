26f9b42fe9b0e6af7fda7c9a1c120d7f80c65c6d
Ibrahim Masri <msri.ibraheem@gmail.com>
2021-07-04 16:48:20 +0300

    Currency : Add WebAPI interface and implementation to fetch data from web apis

---
+++ PriceCalculatorKata/IWebAPI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+
+namespace PriceCalculatorKata
+{
+    public interface IWebAPI
+    {
+        public Task<ImmutableDictionary<string, object>> GetResponse(string endpoint,
+            Dictionary<string, object> requestArguments);
+    }
+
+    class HttpClientWebAPI : IWebAPI
+    {
+        private HttpClient _client;
+
+        public string BaseAddress
+        {
+            get { return _client.BaseAddress.ToString(); }
+            set { _client.BaseAddress = new Uri(value); }
+        }
+
+        public HttpClientWebAPI()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<ImmutableDictionary<string, object>> GetResponse(string endpoint,
+            Dictionary<string, object> requestArguments)
+        {
+            string httpConcatenatedArguments = "";
+            foreach (var argument in requestArguments)
+            {
+                httpConcatenatedArguments += $"{argument.Key}={argument.Value}&";
+            }
+            
+            var response = await _client.GetAsync(endpoint + "?" + httpConcatenatedArguments);
+            response.EnsureSuccessStatusCode();
+            dynamic reponseContent = await response.Content.ReadAsAsync<ExpandoObject>();
+            return ((ImmutableDictionary<string, object>) reponseContent).ToImmutableDictionary();
+        }
+    }
+}
